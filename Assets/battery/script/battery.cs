8fd72c4b2cff390c1372fa6a4efc36f7175c78d0
Andersen <ac941230gmail.com>
2017-03-11 01:03:14 +0800

    pick up battery

--- Assets/battery/script/battery.cs
+++ Assets/battery/script/battery.cs
@@ -7,24 +7,34 @@ public class battery : MonoBehaviour {
 
     public Scrollbar bbattery;
     public float juice = 100;
-    
+    public Text textref;
+    public int max = 10;
+    public static battery Instance;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
    	void Update ()
     {
         GameObject thespotlight = GameObject.FindGameObjectWithTag("Spotlight");
         light ll = thespotlight.GetComponentInChildren<light>();
-        
-        //press "R" to recharge
-        if(Input.GetKeyDown(KeyCode.R))
+
+        //press "R" to recharge 
+        if(Input.GetKeyDown(KeyCode.R) && (max != 0) && (juice < 50))
         {
             juice = 100;
             ll.spotLight.enabled = true;
+            max -= 1;
+            textref.text = max.ToString();
         }
 
         
         if (juice >= 10)
         {
             //battery juice will automatically decrease when the time past
-            float timepast = Time.deltaTime * 10;
+            float timepast = Time.deltaTime;
             juice -= timepast;
             bbattery.size = juice / 100f;
 
@@ -37,6 +47,13 @@ public class battery : MonoBehaviour {
         }
         //to make sure the light turns off when running out of juice
         else
-            ll.spotLight.enabled = false;
+            ll.spotLight.enabled = false;    
     }
+
+    public void pickBattery()
+    {
+        max += 1;
+        textref.text = max.ToString();
+    }
+    
 }
