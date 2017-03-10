7061424a6363a15ad0494a6783adf363ba5547ec
Andersen <ac941230gmail.com>
2017-03-10 14:32:46 +0800

    basic battery features recharge battery by pressing "R" light start to flash when the battery is almost empty

---
+++ Assets/battery/script/battery.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class battery : MonoBehaviour {
+
+    public Scrollbar bbattery;
+    public float juice = 100;
+    
+   	void Update ()
+    {
+        GameObject thespotlight = GameObject.FindGameObjectWithTag("Spotlight");
+        light ll = thespotlight.GetComponentInChildren<light>();
+        
+        //press "R" to recharge
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            juice = 100;
+            ll.spotLight.enabled = true;
+        }
+
+        
+        if (juice >= 10)
+        {
+            //battery juice will automatically decrease when the time past
+            float timepast = Time.deltaTime * 10;
+            juice -= timepast;
+            bbattery.size = juice / 100f;
+
+            //flash light effect  
+            if ((juice >= 10) && (juice <= 30))
+            {
+                ll.spotLightOn = !ll.spotLightOn;
+                ll.spotLight.enabled = ll.spotLightOn;
+            }
+        }
+        //to make sure the light turns off when running out of juice
+        else
+            ll.spotLight.enabled = false;
+    }
+}
---
+++ Assets/control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+    public Light spotLight;
+    public bool spotLightOn;
+    
+    void Start ()
+    {
+        spotLight = GetComponentInChildren<Light>();
+        spotLight.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            spotLightOn = !spotLightOn;
+            spotLight.enabled = spotLightOn;
+        }
+    }
+}
