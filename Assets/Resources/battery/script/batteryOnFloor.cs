fcd6797ce23073f6fa7814c878f7d5554119de86
Yang-Che Teng <askmeifyouwantmyusername@gmail.com>
2017-03-18 21:57:40 +0800

    Added Battery Model, glow effect

--- Assets/Resources/battery/script/batteryOnFloor.cs
+++ Assets/Resources/battery/script/batteryOnFloor.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class batteryOnFloor : MonoBehaviour {
 
+	private Color _glowColor = new Color(0.4170057f, 0.3920848f, 0.7205882f, 1f);
+	private Renderer _renderer;
+	private void Start()
+	{
+		_renderer = GetComponent<Renderer> ();
+	}
+
+	private void Update()
+	{
+		_renderer.material.SetColor ("_EmissionColor", Color.Lerp (Color.black, _glowColor, Mathf.PingPong(Time.time, 1)));
+	}
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
