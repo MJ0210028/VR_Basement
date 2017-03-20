32a1875e9de8eb3a1d7413dbdfb61db73fcaada7
br90218 <askmeifyouwantmyusername@gmail.com>
2017-03-20 16:52:40 +0800

    Fixed minor bug. Added some shit into it

---
+++ Assets/Scripts/BridgeFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeFall : MonoBehaviour {
+
+	private Rigidbody[] rigids;
+	// Use this for initialization
+	void Start () {
+		rigids = GetComponentsInChildren<Rigidbody> ();
+		foreach (Rigidbody rigid in rigids) {
+			rigid.isKinematic = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (!other.CompareTag ("Player"))
+			return;
+		print ("shit");
+		foreach (Rigidbody rigid in rigids) {
+			rigid.isKinematic = false;
+		}
+	}
+
+}
